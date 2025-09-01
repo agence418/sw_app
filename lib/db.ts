4652ae0d7b91c396a3da819e3cd7dbc5de526d80
Benoît PASCAL <pcl.ben.6@gmail.com>
2025-09-01 14:02:36 +0200

    fix: connection with bdd

--- lib/db.ts
+++ lib/db.ts
@@ -4,7 +4,7 @@ import { Pool } from 'pg';
 import { verifyPassword, hashPassword } from './password';
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+    ssl: false
 });
 
 interface DbParticipant {
